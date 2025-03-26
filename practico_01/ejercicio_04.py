67357a3fa549e7275dea6fe26b314da587ccc949
Duarte, Lautaro <164930302+LautiDuarte@users.noreply.github.com>
2025-03-26 18:21:02 -0300

    Update ejercicio_04.py

--- practico_01/ejercicio_04.py
+++ practico_01/ejercicio_04.py
@@ -2,13 +2,17 @@
 
 
 def es_vocal_if(letra: str) -> bool:
-    """Toma un string y devuelve un booleano en base a si letra es una vocal o
-    no.
-
-    Restricción: Utilizar un if para cada posibilidad con la función lower().
-    Referencia: https://docs.python.org/3/library/stdtypes.html#string-methods
-    """
-    pass # Completar
+     if letra.lower() == "a": 
+        return True
+    elif letra[0].lower() == "e": 
+        return True
+    elif letra[0].lower() == "i": 
+        return True
+    elif letra[0].lower() == "o": 
+        return True
+    elif letra[0].lower() == "u": 
+        return True    
+    else: return False
 
 
 # NO MODIFICAR - INICIO
@@ -22,10 +26,11 @@ assert es_vocal_if("A")
 
 
 def es_vocal_if_in(letra: str) -> bool:
-    """Re-escribir utilizando un sólo IF y el operador IN.
-    Referencia: https://docs.python.org/3/reference/expressions.html#membership-test-operations
-    """
-    pass # Completar
+    if letra.lower() in ("a","e","i","o","u"):
+        return True
+    else:
+        return False
+
 
 
 # NO MODIFICAR - INICIO
@@ -39,8 +44,7 @@ assert es_vocal_if_in("A")
 
 
 def es_vocal_in(letra: str) -> bool:
-    """Re-escribir utilizando el operador IN pero sin utilizar IF."""
-    pass # Completar
+    return letra.lower() in "aeiou"
 
 
 # NO MODIFICAR - INICIO
