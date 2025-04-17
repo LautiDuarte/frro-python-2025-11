2827c1d0249de61aec208db26723bfc94be3f284
yamiladonzino <166164256+yamiladonzino@users.noreply.github.com>
2025-04-17 17:28:00 -0300

    Create practico_02_property_test.py

---
+++ practico_02/practico_02_property_test.py
@@ -0,0 +1,10 @@
+def test_asserts_ejercicio_01():
+    import ejercicio_01
+
+
+def test_asserts_ejercicio_02():
+    import ejercicio_02
+
+
+def test_asserts_ejercicio_03():
+    import ejercicio_03
